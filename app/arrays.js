daec532c94a50ea70eb7c2a591438b6f68010a83
Clint Losee <clint.losee@gmail.com>
2015-11-20 16:56:27 -0700

    Solution for remove item from array

--- app/arrays.js
+++ app/arrays.js
@@ -23,7 +23,12 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-
+      for (var i = 0; i < arr.length; i++) {
+          if (arr[i] === item) {
+              arr.splice(i, 1);
+          }
+      }
+      return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
